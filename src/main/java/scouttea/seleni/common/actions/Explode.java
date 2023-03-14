74f84d3082b973180cdba78d97069809027b4eb8
QuantumXenon <thequantumxenon@gmail.com>
2023-03-14 21:55:24 +0000

    Port to 1.19.4
    [Still needs to be tested]

--- src/main/java/scouttea/seleni/common/actions/Explode.java
+++ src/main/java/scouttea/seleni/common/actions/Explode.java
@@ -3,7 +3,7 @@ package scouttea.seleni.common.actions;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.world.World;
-import scouttea.seleni.common.Strings;
+import net.minecraft.world.explosion.Explosion;
 import scouttea.seleni.common.damage_sources.OriginExplosionDamageSource;
 
 // Source: net.minecraft.entity.mob.CreeperEntity
@@ -11,7 +11,7 @@ import scouttea.seleni.common.damage_sources.OriginExplosionDamageSource;
 public class Explode {
     public static void explode(LivingEntity entity, float radius, boolean skull, boolean create_fire, World.ExplosionSourceType explosionSourceType) {
         if (entity.world.isClient()) return;
-        DamageSource source = new OriginExplosionDamageSource(Strings.CREEPER_BOOM_DAMAGE_SOURCE, entity, skull);
+        DamageSource source = new OriginExplosionDamageSource(entity.world.getDamageSources().explosion(null), entity, skull);
         entity.world.createExplosion(entity, source, null, entity.getX(), entity.getY(), entity.getZ(), radius, create_fire, explosionSourceType);
     }
 }
--- src/main/java/scouttea/seleni/common/damage_sources/OriginExplosionDamageSource.java
+++ src/main/java/scouttea/seleni/common/damage_sources/OriginExplosionDamageSource.java
@@ -1,18 +1,17 @@
 package scouttea.seleni.common.damage_sources;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.damage.EntityDamageSource;
+import net.minecraft.entity.damage.DamageSource;
 
 /**
  * Custom damage source to allow detection of charged creeper boom (to enable skull drops, for example)
  */
-public class OriginExplosionDamageSource extends EntityDamageSource {
+public class OriginExplosionDamageSource extends DamageSource {
 
     private final boolean charged;
 
-    public OriginExplosionDamageSource(String name, Entity source, boolean charged) {
-        super(name, source);
-        this.setExplosive();
+    public OriginExplosionDamageSource(DamageSource name, Entity source, boolean charged) {
+        super(name.getTypeRegistryEntry(), source);
         this.charged = charged;
     }
 
