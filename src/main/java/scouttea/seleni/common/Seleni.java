134a3ac0ef2cc7caa1b93872265a2e8c57c1ef75
QuantumXenon <thequantumxenon@gmail.com>
2023-06-19 13:08:17 +0100

    Remove 'Seleni has initialized' message

--- src/main/java/scouttea/seleni/common/Seleni.java
+++ src/main/java/scouttea/seleni/common/Seleni.java
@@ -2,24 +2,17 @@ package scouttea.seleni.common;
 
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import scouttea.seleni.common.registry.ModEntityActions;
 import scouttea.seleni.common.registry.ModPowers;
 
 public class Seleni implements ModInitializer {
-    public static final String MODID = "seleni";
-    public static final Logger LOGGER = LogManager.getLogger("Seleni");
-
     public static Identifier identifier(String path) {
-        return new Identifier(MODID, path);
+        return new Identifier("Seleni", path);
     }
 
     @Override
     public void onInitialize() {
         ModPowers.init();
         ModEntityActions.init();
-
-        LOGGER.info("Seleni has initialized.");
     }
 }
